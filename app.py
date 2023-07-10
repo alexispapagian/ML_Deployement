aece5288a57da5bdbe449b397ca122f7c63009ad
Alexis Papagiannopoulos <alexis.papagiannopoulos@gmail.com>
2023-07-10 12:34:32 +0200

    fixed space

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -13,14 +13,15 @@ def home():
 def predict():
     '''
     For rendering results on HTML GUI
-    ''' 
-    int_features= [int(x) for x in request.form.values()]
+    '''
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0],1)
+    output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text = 'Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
